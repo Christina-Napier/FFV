f4fecd5c361de5de37263cb6b49814f559caacf6
Alex Burbidge <alex@expend.io>
2016-08-28 13:11:34 +0100

    Update bin2dec.py
    Fix bin2dec.py

--- bin2dec.py
+++ bin2dec.py
@@ -1,23 +1,26 @@
 import binascii
-import sys
-import sys
-#sys.stdout=open("test.txt","w")
-filename = 'images.jpg'
-with open(filename, 'rb') as f:
-    content = f.read()
-print(binascii.hexlify(content))
-hexfile =(binascii.b2a_hex(content))
 
-print hexfile
-a =(int(hexfile,16))
-s = str(a)
-a = int(''.join(str(i) for i in s))
-it = iter(s)
-for x in it:
- print(x, next(it))
+def jpeg_to_decimal(jpeg_loc):
+    '''
+    Takes the location of a jpeg and returns it as a list of decimal values.
 
-#sys.stdout.close()
+    :param jpeg_loc: The location of the Jpeg to read
+    :return: A list of decimals representing the jpeg.
+    '''
+    with open(jpeg_loc, 'rb') as f:
+        content = f.read()
+
+    hex = binascii.b2a_hex(content)
+    str_hex = str(hex)[2:]  # Skip the first 2 chars as its the binary flag.
+    split_string_hex = map(''.join, zip(*[iter(str_hex)]*2))
+
+    decimal_values = []
+    for s in split_string_hex:
+        dec = int(s, 16)
+        decimal_values.append(dec)
+        print(dec)
+
+    return decimal_values
 
 
-
-
+jpeg_to_decimal(r'C:\Users\Alex\Downloads\images.jpg')
