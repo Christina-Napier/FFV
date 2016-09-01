c83becf1e10699f0f7608aafc5008dc898e43e33
Christina-Napier <40179349@napier.ac.uk>
2016-09-01 18:13:06 +0100

    Add files via upload

---
+++ updated.py
@@ -0,0 +1,54 @@
+import binascii
+import sys
+import matplotlib
+from pylab import *
+import numpy as np
+sys.stdout=open("test.txt","w")
+
+def jpeg_to_decimal(jpeg_loc, as_rgb=True):
+    '''
+    Takes the location of a jpeg and returns it as a list of decimal values.
+    :param jpeg_loc: The location of the Jpeg to read
+    :param as_rgb: If True, returns rgb tuples.
+    :return: A list of decimals representing the jpeg.
+    '''
+    with open(jpeg_loc, 'rb') as f:
+        content = f.read()
+
+    hex = binascii.b2a_hex(content)
+    split_string_hex = map(''.join, zip(*[iter(hex)]*2))
+
+    decimal_values = []
+    for s in split_string_hex:
+        dec = int(s, 16)
+        decimal_values.append(dec)
+        
+    if as_rgb:
+       
+       t = zip(*[decimal_values[i::3] for i in range(3)])  # This branch gives an iterable
+       print t
+       #getting indentation errors with the below
+    else:
+	return decimal_values # This branch gives a list
+	
+#Create test data with zero valued diagonal:
+	#data = np.random.random_sample((512, 512))
+	#data = What do I put in here to get the data from decimal_values??
+	
+	#rows, cols = np.indices((25,25))
+	#data[np.diag(rows, k=0), np.diag(cols, k=0)] = 0
+
+#Create new colormap, with white for zero 
+#(can also take RGB values, like (255,255,255):
+	#colors = [('white')] + [(cm.jet(i)) for i in xrange(1,256)]
+	#new_map = matplotlib.colors.LinearSegmentedColormap.from_list('new_map', colors, N=256)
+
+	#pcolor(data, cmap=new_map)
+	#colorbar()
+	#savefig('map2.png')
+	#show()
+
+
+jpeg_to_decimal(r'text.txt')
+sys.stdout.close()
+		
